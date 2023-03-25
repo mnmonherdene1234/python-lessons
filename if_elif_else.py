01429bcde4f4d11961864d5ad3776e573ea593fe
mnmonherdene <mnmonherdene1234@gmail.com>
2023-03-25 17:46:44 +0800

    first push

---
+++ if_elif_else.py
@@ -0,0 +1,8 @@
+age = 20
+
+if(age >= 18):
+    print("You're Welcome!")
+elif age < 0:
+    print("You not born yet!")
+else:
+    print("You're a child!")
---
+++ variable.py
@@ -0,0 +1,14 @@
+#Declare variables
+stringVar = "Hello Python"
+intVar = 999
+floatVar = 999.999
+
+#Print variables value
+print(stringVar)
+print(intVar)
+print(floatVar)
+
+#Print variables type
+print(type(stringVar))
+print(type(intVar))
+print(type(floatVar))
